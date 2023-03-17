cb92c790ffa843504334c07feb050c89b6f907c2
vgoer <hi_goer@163.com>
2023-03-17 09:44:53 +0800

    api

--- src/Api/user.js
+++ src/Api/user.js
@@ -2,11 +2,17 @@ import instance from "./request";
 
 
 // 登录请求数据
-export const userLoginApi = (params) => instance.get('/user/login',params);
+// export const userLoginApi = (params) => instance.get('/user/login',params);
 
 // 用户注册请求数据
 export const userRegApi = (params) => instance.post('/user/reg',params);
 
 
-
-
+// 这样写更优雅
+export const userLoginApi = (query) => {
+    instance({
+        url:'/user/login',
+        method:'post',
+        params: query
+    })
+}
